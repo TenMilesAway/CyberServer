ae8a8265c11c1640361bb3e32c1cb0a0458e668a
TenMilesAway <zll749486310@163.com>
2025-12-21 20:13:41 +0800

    【12/21】服务端更新 PlayerData

--- Entity/PlayerData.cs
+++ Entity/PlayerData.cs
@@ -10,24 +10,32 @@ using System.Threading.Tasks;
 [System.Serializable]
 public class PlayerInfo
 {
-    public string _id;
-    public string _name;
-    public string _head;
+    public string _id;                // 玩家 ID
+    public string _name;              // 玩家名称
+    public string _head;              // 玩家头像 (路径)
 
-    public int _level;
-    public int _commonCurrency;
-    public int _rareCurrency;
-    public int _maxHP;
-    public int _currentHP;
-    public int _maxMP;
-    public int _currentMP;
-    public int _maxEXP;
-    public int _currentEXP;
+    public int _level;                // 玩家等级
+    public int _commonCurrency;       // 玩家普通货币数
+    public int _rareCurrency;         // 玩家稀有货币数
+    public int _maxEXP;               // 玩家最大经验值
+    public int _currentEXP;           // 玩家当前经验值
+    public int _maxHP;                // 玩家最大血量
+    public int _currentHP;            // 玩家当前血量
+    public int _maxMP;                // 玩家最大灵力
+    public int _currentMP;            // 玩家当前灵力
+    public int _pAttack;              // 玩家攻击力
+    public int _pArmorPenetration;    // 玩家破甲值
+    public int _pDefense;             // 玩家防御值
+    public int _pDamageAvoidance;     // 玩家免伤值
 
-    public List<ItemInfo> _items;
-    public List<ItemInfo> _equips;
-    public List<ItemInfo> _potions;
-    public List<ItemInfo> _nowEquips;
+    public List<ItemInfo> _items;     // 玩家拥有物品
+    public List<ItemInfo> _equips;    // 玩家拥有装备
+    public List<ItemInfo> _potions;   // 玩家拥有药水
+    public List<ItemInfo> _nowEquips; // 玩家当前已装备
+    public int _inventoryItemNum;     // 玩家仓库：物品格子数
+    public int _inventoryEquipNum;    // 玩家仓库：装备格子数
+    public int _inventoryPotionNum;   // 玩家仓库：药水格子数
+    public int _safeboxNum;           // 安全行囊格子数
 }
 
 [System.Serializable]
