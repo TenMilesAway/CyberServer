77bac0a2dc37da81302cd466903a4cd7ba65c0db
TenMilesAway <zll749486310@163.com>
2025-12-25 22:36:44 +0800

    【12/25】增加新玩家属性

--- Entity/PlayerData.cs
+++ Entity/PlayerData.cs
@@ -10,30 +10,34 @@ using System.Threading.Tasks;
 [System.Serializable]
 public class PlayerInfo
 {
-    public string _id;                // 玩家 ID
-    public string _name;              // 玩家名称
-    public string _head;              // 玩家头像 (路径)
+    public string _id;                  // 玩家 ID
+    public string _name;                // 玩家名称
+    public string _head;                // 玩家头像 (路径)
+                                        
+    public int _level;                  // 玩家等级
+    public int _commonCurrency;         // 玩家普通货币数
+    public int _rareCurrency;           // 玩家稀有货币数
+    public int _maxEXP;                 // 玩家最大经验值
+    public int _currentEXP;             // 玩家当前经验值
+    public int _maxHP;                  // 玩家最大血量
+    public int _currentHP;              // 玩家当前血量
+    public int _maxMP;                  // 玩家最大灵力
+    public int _currentMP;              // 玩家当前灵力
+    public int _pAttack;                // 玩家攻击力
+    public int _pArmorPenetration;      // 玩家破甲值
+    public int _pDefense;               // 玩家防御值
+    public int _pDamageAvoidance;       // 玩家免伤值
+    public float _pCriticalProbability; // 玩家暴击率
+    public float _pCriticalMultiplier;  // 玩家暴击倍率
+    public float _pSuckProbability;     // 玩家吸血率
+    public float _pSuckMultiplier;      // 玩家吸血倍率
 
-    public int _level;                // 玩家等级
-    public int _commonCurrency;       // 玩家普通货币数
-    public int _rareCurrency;         // 玩家稀有货币数
-    public int _maxEXP;               // 玩家最大经验值
-    public int _currentEXP;           // 玩家当前经验值
-    public int _maxHP;                // 玩家最大血量
-    public int _currentHP;            // 玩家当前血量
-    public int _maxMP;                // 玩家最大灵力
-    public int _currentMP;            // 玩家当前灵力
-    public int _pAttack;              // 玩家攻击力
-    public int _pArmorPenetration;    // 玩家破甲值
-    public int _pDefense;             // 玩家防御值
-    public int _pDamageAvoidance;     // 玩家免伤值
-
-    public List<ItemInfo> _nowEquips; // 玩家当前已装备
-    public List<ItemInfo> _allItems;  // 玩家拥有的所有物品
-    public int _inventoryItemNum;     // 玩家仓库：物品格子数
-    public int _inventoryEquipNum;    // 玩家仓库：装备格子数
-    public int _inventoryPotionNum;   // 玩家仓库：药水格子数
-    public int _safeboxNum;           // 安全行囊格子数
+    public List<ItemInfo> _nowEquips;   // 玩家当前已装备
+    public List<ItemInfo> _allItems;    // 玩家拥有的所有物品
+    public int _inventoryItemNum;       // 玩家仓库：物品格子数
+    public int _inventoryEquipNum;      // 玩家仓库：装备格子数
+    public int _inventoryPotionNum;     // 玩家仓库：药水格子数
+    public int _safeboxNum;             // 安全行囊格子数
 }
 
 [System.Serializable]
