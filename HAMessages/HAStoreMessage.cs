b0ebe39d5efddb451c2d3b8ffc6146d506126229
TenMilesAway <zll749486310@163.com>
2026-01-12 17:58:22 +0800

    【1/12】新增商店协议

---
+++ HAMessages/HAStoreMessage.cs
@@ -0,0 +1,16 @@
+﻿public class HAMsgStoreBuyItem : MsgBase
+{
+    public HAMsgStoreBuyItem() { protoName = "HAMsgStoreBuyItem"; }
+
+    // 目前未在服务端进行校验
+    public string playerID;
+    public int price;
+    public int currencyType; // 0灵晶币 1灵星石
+    public int itemID;
+    public int itemNum;
+
+    /// <summary>
+    /// 回复: 0成功 1失败
+    /// </summary>
+    public int result = 0;
+}
---
+++ Logic/HAStoreMsgHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CyberServer;
+
+public partial class MsgHandler
+{
+    public static void HAMsgStoreBuyItem(ClientState c, MsgBase msgBase)
+    {
+        HAMsgStoreBuyItem msg = (HAMsgStoreBuyItem)msgBase;
+
+        PlayerBaseEntity baseInfo = DBManager.SelectPlayerBase(msg.playerID);
+
+        if (baseInfo == null)
+        {
+            msg.result = 1;
+            NetManager.Send(c, msg);
+            LogService.Error("[服务器] 购买物品失败，用户基础信息不存在" + c.socket.RemoteEndPoint);
+            return;
+        }
+
+        // 检查货币是否足够
+        if (msg.currencyType == 0) // 普通货币
+        {
+            if (baseInfo.common_currency < msg.price)
+            {
+                msg.result = 1; // 货币不足
+                NetManager.Send(c, msg);
+                LogService.Info("[服务器] 购买物品失败，普通货币不足" + c.socket.RemoteEndPoint);
+                return;
+            }
+            baseInfo.common_currency -= msg.price;
+            DBManager.UpdatePlayerBase(baseInfo);
+            msg.result = 0;
+            NetManager.Send(c, msg);
+        }
+        else if (msg.currencyType == 1) // 稀有货币
+        {
+            if (baseInfo.rare_currency < msg.price)
+            {
+                msg.result = 1; // 货币不足
+                NetManager.Send(c, msg);
+                LogService.Info("[服务器] 购买物品失败，稀有货币不足" + c.socket.RemoteEndPoint);
+                return;
+            }
+            baseInfo.rare_currency -= msg.price;
+            DBManager.UpdatePlayerBase(baseInfo);
+            msg.result = 0;
+            NetManager.Send(c, msg);
+        }
+    }
+}
